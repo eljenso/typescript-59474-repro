6df61d6cfd331a8553356df71a63503657d4e6f7
Jens Böttcher <eljenso.boettcher@gmail.com>
2024-11-05 19:55:43 +0100

    Add example MUI code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { Stack, Button } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+function App(): JSX.Element {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button onClick={(event) => console.log({ event })} variant="text">
+        Text
+      </Button>
+      <Button onClick={(event) => console.log({ event })} variant="contained">
+        Contained
+      </Button>
+      <Button onClick={(event) => console.log({ event })} variant="outlined">
+        Outlined
+      </Button>
+    </Stack>
+  );
+}
