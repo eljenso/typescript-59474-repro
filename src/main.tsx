44dfe483e3d43930b65a9e592bc3fdfbf9cbfeb0
Jens Böttcher <eljenso.boettcher@gmail.com>
2024-11-05 20:00:07 +0100

    Use r3f

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Stack, Button } from "@mui/material";
+import { Canvas } from "@react-three/fiber";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Buttons />
+
+    <ThreeCanvas />
   </StrictMode>
 );
 
-function App(): JSX.Element {
+function Buttons(): JSX.Element {
   return (
     <Stack spacing={2} direction="row">
       <Button onClick={(event) => console.log({ event })} variant="text">
@@ -24,5 +27,18 @@ function App(): JSX.Element {
         Outlined
       </Button>
     </Stack>
+  );
+}
+
+function ThreeCanvas(): JSX.Element {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshPhongMaterial />
+      </mesh>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} color="red" />
+    </Canvas>
   );
 }
